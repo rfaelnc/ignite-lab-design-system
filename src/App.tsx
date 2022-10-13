37e77393db05512105e78f6f5178f3c9ab382d94
Rafael C <rfaelnc@gmail.com>
2022-10-12 21:39:38 -0300

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import './styles/global.css';
+
+export function App() {
+  return (
+    <>
+      <h1 className="font-bold text-5xl text-violet-500">Hello World</h1>
+      <button className="bg-cyan-500 font-medium px-4 py-y rounded text-white hover:bg-violet-600">
+        Enviar
+      </button>
+    </>
+  );
+}
